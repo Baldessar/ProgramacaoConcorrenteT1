ec375bd9abab86694abf65bb2e71bd488660271c
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-04-29 18:04:30 -0300

    Modificado aloca_aviao e desaloca_aviao

--- src/aviao.c
+++ src/aviao.c
@@ -1,13 +1,18 @@
 #include "aviao.h"
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * aviao.c
  * Implementação das funções do aviao.h
  * Descrições em aviao.h
  **/
-
+int id = 0;
 aviao_t * aloca_aviao (size_t combustivel, size_t id) {
-  return NULL;
+  aviao_t aviao = (struct aviao_t*) malloc (sizeof(aviao_t));
+  aviao->combustivel = (rand() % 100)+1;
+  aviao->id = ++i;
+  pthread_create(aviao->thread, NULL, aproximacao_aeroporto, NULL);
+  return aviao;
 }
 
 void desaloca_aviao(aviao_t* aviao) {
