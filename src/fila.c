26efd0d268c272dfed0fa46e300d1483ce264404
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-04-29 20:11:37 -0300

    Adicionado aloca_elemento, desaloca_elemento, criar_fila, desaloca_fila, inserir

--- src/fila.c
+++ src/fila.c
@@ -7,23 +7,61 @@
  **/
 
  elemento_t * aloca_elemento (aviao_t * dado) {
-    return NULL;
+    
+    elemento_t* elemento = (elemento_t*) malloc(sizeof(elemento_t));
+    
+    elemento->anterior = NULL;
+    elemento->proximo = NULL;
+    elemento->dado = dado;
+
+    return elemento;
  }
 
  void desaloca_elemento (elemento_t * elemento) {
-
+    free(elemento);
  }
 
 fila_ordenada_t * criar_fila () {
-    return NULL;
+    fila_ordenada_t fila = (fila_ordenada_t*) malloc(sizeof(fila_ordenada_t));
+
+    fila->primeiro = NULL;
+    fila->ultimo = NULL;
+    fila->n_elementos = 0;
+
+    return fila;
+    
 }
 
 void desaloca_fila (fila_ordenada_t * fila) {
-
+    free(fila);
 }
 
 void inserir (fila_ordenada_t * fila, aviao_t * dado) {
-
+    elemento_t* elemento = aloca_elemento(dado);
+    elemento->dado = dado;
+    if (fila->n_elementos == 0)
+    {
+        elemento->proximo = elemento;
+        elemento->anterior = elemento; 
+        fila->primeiro = elemento;
+        fila->ultimo = elemento;
+        fila->n_elementos = 1;
+    } else {
+    	if (dado->combustivel <= 10)
+    	{	
+            elemento->proximo = fila->ultimo;
+            elemento->anterior = fila->primeiro;
+            fila->primeiro->proximo = elemento;
+            fila->primeiro = elemento;
+    	} else {
+            elemento->proximo = fila->ultimo;
+            elemento->anterior = fila->primeiro;
+            fila->primeiro->proximo = elemento;
+            fila->ultimo->anterior = elemento;
+            fila->ultimo = elemento->
+    	}
+        fila->n_elementos++;
+    }
 }
 
 aviao_t * remover (fila_ordenada_t * fila) {
