b7219d4b4212e8cb2b94ccd56476c3ca8a285442
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-05-01 23:08:36 -0300

    Modificado main.c

--- src/main.c
+++ src/main.c
@@ -16,86 +16,111 @@
 
 int main (int argc, char** argv) {
 
-	// Variáveis temporais (inicio t_)
-	size_t t_novo_aviao_min, t_novo_aviao_max;
-	size_t t_pouso_decolagem;
-	size_t t_remover_bagagens, t_inserir_bagagens;
-	size_t t_bagagens_esteira, t_simulacao;
+    //parameter struct
+    typedef struct
+    {
+        aeroporto_t aeroporto;
+        aviao_t aviao;
+        
+    }parametros_aviao;
 
-	// Variáveis discretas (inicio n_)
-	size_t n_pistas, n_portoes;
-	size_t n_max_avioes_esteira, n_esteiras;
-	size_t n_args;
+    // Variáveis temporais (inicio t_)
+    size_t t_novo_aviao_min, t_novo_aviao_max;
+    size_t t_pouso_decolagem;
+    size_t t_remover_bagagens, t_inserir_bagagens;
+    size_t t_bagagens_esteira, t_simulacao;
 
-	// Variáveis de prioridade (inicio p_)
-	size_t p_combustivel_min, p_combustivel_max;
+    // Variáveis discretas (inicio n_)
+    size_t n_pistas, n_portoes;
+    size_t n_max_avioes_esteira, n_esteiras;
+    size_t n_args;
 
-	if (argc == 5) { // Argumentos sem tempos de execução
-		t_novo_aviao_min = NOVO_AVIAO_MIN;
-		t_novo_aviao_max = NOVO_AVIAO_MAX;
-		t_pouso_decolagem = TEMPO_POUSO_DECOLAGEM;
-		t_remover_bagagens = TEMPO_REMOVER_BAGAGENS;
-		t_inserir_bagagens = TEMPO_INSERIR_BAGAGENS;
-		t_bagagens_esteira = TEMPO_BAGAGENS_ESTEIRA;
-		t_simulacao = TEMPO_SIMULACAO;
-		p_combustivel_min = COMBUSTIVEL_MIN;
-		p_combustivel_max = COMBUSTIVEL_MAX;
-		n_pistas = atoi(argv[1]);
-		n_portoes = atoi(argv[2]);
-		n_max_avioes_esteira = atoi(argv[3]);
-		n_esteiras = atoi(argv[4]);
+    // Variáveis de prioridade (inicio p_)
+    size_t p_combustivel_min, p_combustivel_max;
 
-	} else if (argc == 14) { // Argumentos com tempos de execução
-		int i = 0; // Este contador será incrementado antes de coletar um argumento
-		t_novo_aviao_min = atoi(argv[++i]);
-		t_novo_aviao_max = atoi(argv[++i]);
-		p_combustivel_min = atoi(argv[++i]);
-		p_combustivel_max = atoi(argv[++i]);
-		t_pouso_decolagem = atoi(argv[++i]);
-		n_pistas = atoi(argv[++i]);
-		t_remover_bagagens = atoi(argv[++i]);
-		t_inserir_bagagens = atoi(argv[++i]);
-		n_portoes = atoi(argv[++i]);
-		n_max_avioes_esteira = atoi(argv[++i]);
-		t_bagagens_esteira = atoi(argv[++i]);
-		n_esteiras = atoi(argv[++i]);
-		t_simulacao = atoi(argv[++i]);
+    if (argc == 5) { // Argumentos sem tempos de execução
+        t_novo_aviao_min = NOVO_AVIAO_MIN;
+        t_novo_aviao_max = NOVO_AVIAO_MAX;
+        t_pouso_decolagem = TEMPO_POUSO_DECOLAGEM;
+        t_remover_bagagens = TEMPO_REMOVER_BAGAGENS;
+        t_inserir_bagagens = TEMPO_INSERIR_BAGAGENS;
+        t_bagagens_esteira = TEMPO_BAGAGENS_ESTEIRA;
+        t_simulacao = TEMPO_SIMULACAO;
+        p_combustivel_min = COMBUSTIVEL_MIN;
+        p_combustivel_max = COMBUSTIVEL_MAX;
+        n_pistas = atoi(argv[1]);
+        n_portoes = atoi(argv[2]);
+        n_max_avioes_esteira = atoi(argv[3]);
+        n_esteiras = atoi(argv[4]);
 
-	} else { // Número incorreto de argumentos
-		printf("Todas as entradas são inteiros positivos!!\nUso:\n");
-		printf("./aeroporto  NOVO_AVIAO_MIN  NOVO_AVIAO_MAX\n");
-		printf("COMBUSTIVEL_MIN COMBUSTIVEL_MAX\n");
-		printf("TEMPO_POUSO_DECOLAGEM  NUMERO_PISTAS  TEMPO_REMOVER_BAGAGENS\n");
-		printf("TEMPO_INSERIR_BAGAGENS  NUMERO_PORTOES  MAXIMO_AVIOES_ESTEIRA\n");
-		printf("TEMPO_BAGAGENS_ESTEIRA  NUMERO_ESTEIRAS  TEMPO_SIMULACAO\n");
-		printf("----------OU----------\n");
-		printf("./airport  NUMERO_PISTAS  NUMERO_PORTOES  MAXIMO_AVIOES_ESTEIRA  NUMERO_ESTEIRAS\n");
-		return 0;
-	}
+    } else if (argc == 14) { // Argumentos com tempos de execução
+        int i = 0; // Este contador será incrementado antes de coletar um argumento
+        t_novo_aviao_min = atoi(argv[++i]);
+        t_novo_aviao_max = atoi(argv[++i]);
+        p_combustivel_min = atoi(argv[++i]);
+        p_combustivel_max = atoi(argv[++i]);
+        t_pouso_decolagem = atoi(argv[++i]);
+        n_pistas = atoi(argv[++i]);
+        t_remover_bagagens = atoi(argv[++i]);
+        t_inserir_bagagens = atoi(argv[++i]);
+        n_portoes = atoi(argv[++i]);
+        n_max_avioes_esteira = atoi(argv[++i]);
+        t_bagagens_esteira = atoi(argv[++i]);
+        n_esteiras = atoi(argv[++i]);
+        t_simulacao = atoi(argv[++i]);
 
-	// Impressão com os parâmetros selecionados para simulação
-	printf("Simulação iniciada com tempo total: %lu\n", t_simulacao);
-	printf("Tempo para criação de aviões: %lu - %lu\n", t_novo_aviao_min, t_novo_aviao_max);
-	printf("Número de pistas de pouso: %lu\n", n_pistas);
-	printf("Tempo de pouso e decolagem: %lu\n", t_pouso_decolagem);
-	printf("Número de portões de embarque: %lu\n", n_portoes);
-	printf("Tempo de inserção (%lu) e remoção (%lu) de bagagens\n", t_inserir_bagagens, t_remover_bagagens);
-	printf("Número de esteiras: %lu, com %lu aviões por esteira\n", n_esteiras, n_max_avioes_esteira);
-	printf("Tempo das bagagens nas esteiras: %lu\n", t_bagagens_esteira);
+    } else { // Número incorreto de argumentos
+        printf("Todas as entradas são inteiros positivos!!\nUso:\n");
+        printf("./aeroporto  NOVO_AVIAO_MIN  NOVO_AVIAO_MAX\n");
+        printf("COMBUSTIVEL_MIN COMBUSTIVEL_MAX\n");
+        printf("TEMPO_POUSO_DECOLAGEM  NUMERO_PISTAS  TEMPO_REMOVER_BAGAGENS\n");
+        printf("TEMPO_INSERIR_BAGAGENS  NUMERO_PORTOES  MAXIMO_AVIOES_ESTEIRA\n");
+        printf("TEMPO_BAGAGENS_ESTEIRA  NUMERO_ESTEIRAS  TEMPO_SIMULACAO\n");
+        printf("----------OU----------\n");
+        printf("./airport  NUMERO_PISTAS  NUMERO_PORTOES  MAXIMO_AVIOES_ESTEIRA  NUMERO_ESTEIRAS\n");
+        return 0;
+    }
 
-	// Inicialização do aeroporto
-	n_args = 8;
-	size_t args[8] = {n_pistas, n_portoes, n_esteiras,
-				n_max_avioes_esteira,
-				t_pouso_decolagem, t_remover_bagagens,
-				t_inserir_bagagens, t_bagagens_esteira};
+    // Impressão com os parâmetros selecionados para simulação
+    printf("Simulação iniciada com tempo total: %lu\n", t_simulacao);
+    printf("Tempo para criação de aviões: %lu - %lu\n", t_novo_aviao_min, t_novo_aviao_max);
+    printf("Número de pistas de pouso: %lu\n", n_pistas);
+    printf("Tempo de pouso e decolagem: %lu\n", t_pouso_decolagem);
+    printf("Número de portões de embarque: %lu\n", n_portoes);
+    printf("Tempo de inserção (%lu) e remoção (%lu) de bagagens\n", t_inserir_bagagens, t_remover_bagagens);
+    printf("Número de esteiras: %lu, com %lu aviões por esteira\n", n_esteiras, n_max_avioes_esteira);
+    printf("Tempo das bagagens nas esteiras: %lu\n", t_bagagens_esteira);
 
-	aeroporto_t* meu_aeroporto = iniciar_aeroporto(args, n_args);
+    // Inicialização do aeroporto
+    n_args = 8;
+    size_t args[8] = {n_pistas, n_portoes, n_esteiras,
+                n_max_avioes_esteira,
+                t_pouso_decolagem, t_remover_bagagens,
+                t_inserir_bagagens, t_bagagens_esteira};
 
-	// Descreve aqui sua simulação usando as funções definidas no arquivo "aeroporto.h"
-	// Lembre-se de implementá-las num novo arquivo "aeroporto.c"
+    aeroporto_t* meu_aeroporto = iniciar_aeroporto(args, n_args);
 
+    // Descreve aqui sua simulação usando as funções definidas no arquivo "aeroporto.h"
+    // Lembre-se de implementá-las num novo arquivo "aeroporto.c"
+    srand(clock());
+    int proxAviao = ((rand() % (NOVO_AVIAO_MAX- NOVO_AVIAO_MIN)) + NOVO_AVIAO_MIN);
+    int chegadaAviao;
+    for (int i = 0; i < TEMPO_SIMULACAO; ++i)
+    {
+        if (chegadaAviao == proxAviao);
+        {   int combustivel = ((rand() % (COMBUSTIVEL_MAX - COMBUSTIVEL_MIN)) + COMBUSTIVEL_MIN);
+            aviao_t novoAviao = aloca_aviao(combustivel,i);
+            parametros_aviao* params = (parametros_aviao*) malloc(sizeof(parametros_aviao));
+            params->aviao = novoAviao;
+            params->aeroporto = meu_aeroporto;
+            pthread_create(aviao->thread, NULL, aproximacao_aeroporto, params);
+            chegadaAviao = 0;
+            proxAviao = ((rand() % (NOVO_AVIAO_MAX- NOVO_AVIAO_MIN)) + NOVO_AVIAO_MIN);
+            
+        }
+        chegadaAviao++;
+    }
 
-	finalizar_aeroporto(meu_aeroporto);
-	return 1;
+    finalizar_aeroporto(meu_aeroporto);
+    return 1;
 }
